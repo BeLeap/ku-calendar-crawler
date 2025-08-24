8ee793c630c977c1e82a6d0f5b868ea1facdbe61
BeLeap <beleap@beleap.dev>
2025-08-24 14:23:49 +0900

    send request to schedule page properly

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,12 @@ import Network.HTTP.Simple
 
 main :: IO ()
 main = do
-  response <- httpLBS "https://registrar.korea.ac.kr/eduinfo/affairs/schedule.do?cYear=2025&hakGi=2&srCategoryId1=1613"
+  request' <- parseRequest "https://registrar.korea.ac.kr/eduinfo/affairs/schedule.do"
+  let request
+        = setRequestMethod "GET"
+        $ setRequestQueryString [("cYear", Just "2025"), ("hakGi", Just "2"), ("srCategoryId1", Just "1613")]
+        $ setRequestHeaders [("User-Agent", "curl/8.14.1")]
+        $ request'
+  response <- httpLBS request
   putStrLn $ "ResCode: " ++ show (getResponseStatusCode response)
+  putStrLn $ "Response Body: " ++ show (getResponseBody response)
