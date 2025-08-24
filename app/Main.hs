f85a13773bb4ba6978b3902c530b58615a333a07
BeLeap <beleap@beleap.dev>
2025-08-24 13:00:01 +0900

    Add http request

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Network.HTTP
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  result <- simpleHTTP (getRequest "https://example.com")
+  case result of
+    Left err -> putStrLn $ "Error: " ++ show err
+    Right response -> putStrLn $ "Response Code:" ++ show (rspCode response)
